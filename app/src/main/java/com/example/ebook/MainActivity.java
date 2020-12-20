cab9a2f5202ac0fc6ff811a09ddd34d45a5f80f9
DesmondSol <51263712+DesmondSol@users.noreply.github.com>
2020-12-20 12:38:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ebook/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.ebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+public class MainActivity extends AppCompatActivity {
+    private PDFView pdfview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        pdfview = findViewById(R.id.pdfViewer);
+
+        pdfview.useBestQuality(true);
+        pdfview.enableSwipe(true);
+        pdfview.fitToWidth();
+        pdfview.fromAsset("kotlinbook.pdf").load();
+
+    }
+}
